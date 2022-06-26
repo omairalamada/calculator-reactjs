5bf2b4a1787f716dcfb50095d4084be9aee88dd9
omairalamada <omairmangondaya@gmail.com>
2022-06-26 15:16:03 +0800

    add useState for state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,38 @@
+import { useContext } from "react"
+import { CalcContext } from "../context/CalcContext"
+
+const getStyleName = btn => {
+    const className = {
+        '=': 'equals',
+        'x': 'opt',
+        '-': 'opt',
+        '+': 'opt',
+        '/': 'opt',
+    }
+
+    return className[btn]
+}
 
 const Button = ({ value }) => {
+    const {calc, setCalc} = useContext(CalcContext);    
+
+// When user click the comma
+    const commaClick = () => {
+        setCalc({
+            ...calc,
+            number: 29
+        })
+    }
+
+    const buttonClick = () => {
+        const results = {
+            '.': commaClick
+        }
+        return results[value]()
+    }
+
     return (
-        <button className="button"> {value}</button>
+        <button onClick={buttonClick} className={`${getStyleName(value)} button`}> {value}</button>
     )
 }
 
--- src/components/ButtonBox.js
+++ src/components/ButtonBox.js
@@ -1,7 +1,7 @@
 
 const ButtonBox = ({ children }) => {
     return (
-        <div className="buttonBox">{ children }</div>
+        <div className="buttonBox">{children}</div>
     )
 } 
 
