38f0fca125e2f4808a6301b26ac4813e7e3fae75
omairalamada <omairmangondaya@gmail.com>
2022-06-26 13:19:55 +0800

    add values for each button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, 1, 3],
+];
 
 function App() {
   return (
     <div>
       <Wrapper>
         <Screen/>
-        <h1>App</h1>
+       <ButtonBox>
+        {btnValues.flat().map((btn, i) => (
+          <Button 
+            values={btn}
+            key={i}
+          />
+        ))}
+       </ButtonBox>
       </Wrapper>
     </div>
   );
